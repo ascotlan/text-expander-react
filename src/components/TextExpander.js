34650c601a61633f9f6fa60da9cc2186dabfd5ea
ascotlan <antonio.k.scotland@gmail.com>
2023-10-16 21:15:17 -0400

    Implement a text expander component

---
+++ src/components/TextExpander.js
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+
+export default function TextExpander({
+  children,
+  collapsedNumWords = 10,
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+  buttonColor = "blue",
+  expanded = false,
+  className,
+}) {
+const [isExpanded, setIsExpanded] = useState(expanded);
+
+const handleExpansion = () => {
+  setIsExpanded((current) => !current);
+}
+
+
+  const buttonStyle = {
+    backgroundColor: "transparent",
+    backgroundRepeat: "no-repeat",
+    border: "none",
+    cursor: "pointer",
+    overflow: "hidden",
+    outline: "none",
+    color: `${buttonColor}`,
+    marginLeft: '8px'
+  };
+
+  const shortenedText =
+    children
+      .split(" ")
+      .slice(0, collapsedNumWords)
+      .join(" ") + "...";
+  return (
+    <div className={className}>
+      <span>{isExpanded ? children : shortenedText}</span>
+      <button onClick={handleExpansion} style={buttonStyle}>
+        {isExpanded ? collapseButtonText : expandButtonText}
+      </button>
+    </div>
+  );
+}
